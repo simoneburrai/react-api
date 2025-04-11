5a84e9bfe14b9ce1d56b47fd4b76dd5a1545e486
simoneburrai <burrai.s98@gmail.com>
2025-04-11 15:19:07 +0200

    feat: mapped all actresses, painted cards on webpage

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const urlActress = "https://freetestapi.com/api/v1/actresses"
+const urlActress = "https://www.freetestapi.com/api/v1/actresses"
 
 
 const Main = () => {
@@ -22,6 +22,18 @@ const Main = () => {
 
     return <main>
         <div className="card-container">
+            {actressList.map(actress => {
+                return <div key={actress.id} className="actress">
+                    <h2 className="name">Name: {actress.name}</h2>
+                    <div className="year">Birth Year:{actress.birth_year}</div>
+                    <div className="nationality">Nationality: {actress.nationality}</div>
+                    <div className="biography">
+                        <h3>Biography</h3>
+                        <p>{actress.biography}</p>
+                    </div>
+                    <img src={actress.image} alt={actress.name} className="img" />
+                </div>
+            })}
         </div>
     </main>
 }
--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     '/api': { // Questo è il percorso che userai nel tuo codice React
-  //       target: 'https://freetestapi.com', // L'URL base dell'API esterna
-  //       changeOrigin: true, // Fondamentale per evitare errori CORS
-  //       rewrite: (path) => path.replace(/^\/api/, '/api/v1'), // Riscrive il percorso se necessario
-  //     },
-  //   },
-  // },
 });
